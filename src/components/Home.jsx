4d82a0d1dcf70ebc4701412a962fec8f0310b8fe
NinoTraverso <njt98git@proton.me>
2024-03-28 16:40:39 +0000

    added some css and centered welcome text in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,20 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1>HOME PAGE</h1>
+      <div id="solarSystem" className="mx-5">
+        <div id="sun"></div>
+        <div id="mercury" class="planet"></div>
+        <div id="venus" class="planet"></div>
+        <div id="earth" class="planet"></div>
+        <div id="mars" class="planet"></div>
+        <div id="jupiter" class="planet"></div>
+        <div id="saturn" class="planet"></div>
+        <div id="uranus" class="planet"></div>
+        <div id="neptune" class="planet"></div>
+      </div>
+      <h1 className="loader d-flex justify-content-center m-5">
+        Welcome to CometOwl
+      </h1>
     </div>
   );
 }
