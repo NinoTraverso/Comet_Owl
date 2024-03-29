1f3df845907795cccd4106f3d53338ecb286dbdf
NinoTraverso <njt98git@proton.me>
2024-03-29 11:26:47 +0000

    started working on home content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,81 +5,26 @@ function Home() {
     <div>
       <div id="solarSystem" className="mx-5">
         <div id="sun"></div>
-        <div id="mercury" class="planet"></div>
-        <div id="venus" class="planet"></div>
-        <div id="earth" class="planet"></div>
-        <div id="mars" class="planet"></div>
-        <div id="jupiter" class="planet"></div>
-        <div id="saturn" class="planet"></div>
-        <div id="uranus" class="planet"></div>
-        <div id="neptune" class="planet"></div>
+        <div id="mercury" className="planet"></div>
+        <div id="venus" className="planet"></div>
+        <div id="earth" className="planet"></div>
+        <div id="mars" className="planet"></div>
+        <div id="jupiter" className="planet"></div>
+        <div id="saturn" className="planet"></div>
+        <div id="uranus" className="planet"></div>
+        <div id="neptune" className="planet"></div>
       </div>
       <h1 className="loader d-flex justify-content-center m-5">
         Welcome to CometOwl
       </h1>
-      <h4>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos corrupti
-        tenetur illo laudantium sit ratione cupiditate ut perferendis quae,
-        animi quo! Harum accusamus perspiciatis nulla iste in repellendus
-        ducimus eius! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Consequatur, voluptates corporis. Sapiente numquam quae eos et magnam,
-        ex esse impedit suscipit non nisi modi perspiciatis quo, minus tempora.
-        Nesciunt, aliquid.
-      </h4>
-      <h4>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos corrupti
-        tenetur illo laudantium sit ratione cupiditate ut perferendis quae,
-        animi quo! Harum accusamus perspiciatis nulla iste in repellendus
-        ducimus eius! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Consequatur, voluptates corporis. Sapiente numquam quae eos et magnam,
-        ex esse impedit suscipit non nisi modi perspiciatis quo, minus tempora.
-        Nesciunt, aliquid.
-      </h4>
-      <h4>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos corrupti
-        tenetur illo laudantium sit ratione cupiditate ut perferendis quae,
-        animi quo! Harum accusamus perspiciatis nulla iste in repellendus
-        ducimus eius! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Consequatur, voluptates corporis. Sapiente numquam quae eos et magnam,
-        ex esse impedit suscipit non nisi modi perspiciatis quo, minus tempora.
-        Nesciunt, aliquid.
-      </h4>
-      <h4>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos corrupti
-        tenetur illo laudantium sit ratione cupiditate ut perferendis quae,
-        animi quo! Harum accusamus perspiciatis nulla iste in repellendus
-        ducimus eius! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Consequatur, voluptates corporis. Sapiente numquam quae eos et magnam,
-        ex esse impedit suscipit non nisi modi perspiciatis quo, minus tempora.
-        Nesciunt, aliquid.
-      </h4>
-      <h4>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos corrupti
-        tenetur illo laudantium sit ratione cupiditate ut perferendis quae,
-        animi quo! Harum accusamus perspiciatis nulla iste in repellendus
-        ducimus eius! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Consequatur, voluptates corporis. Sapiente numquam quae eos et magnam,
-        ex esse impedit suscipit non nisi modi perspiciatis quo, minus tempora.
-        Nesciunt, aliquid.
-      </h4>
-      <h4>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos corrupti
-        tenetur illo laudantium sit ratione cupiditate ut perferendis quae,
-        animi quo! Harum accusamus perspiciatis nulla iste in repellendus
-        ducimus eius! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Consequatur, voluptates corporis. Sapiente numquam quae eos et magnam,
-        ex esse impedit suscipit non nisi modi perspiciatis quo, minus tempora.
-        Nesciunt, aliquid.
-      </h4>
-      <h4>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos corrupti
-        tenetur illo laudantium sit ratione cupiditate ut perferendis quae,
-        animi quo! Harum accusamus perspiciatis nulla iste in repellendus
-        ducimus eius! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Consequatur, voluptates corporis. Sapiente numquam quae eos et magnam,
-        ex esse impedit suscipit non nisi modi perspiciatis quo, minus tempora.
-        Nesciunt, aliquid.
-      </h4>
+      <div>
+        <div className="pelicanNebulaImg">
+          <img
+            src="./assets/space-images/northAmericanPelicanNebula.jpg"
+            alt="North American and Pelican Nebula"
+          />
+        </div>
+      </div>
     </div>
   );
 }
