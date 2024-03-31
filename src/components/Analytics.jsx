a673add5f3323066fb5890d415ae444f38a52a70
NinoTraverso <njt98git@proton.me>
2024-03-31 17:37:25 +0000

    changed back analytics

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,11 +1,6 @@
-import React from "react";
 import { Analytics } from "@vercel/analytics/react";
 import { inject } from "@vercel/analytics";
 
 inject();
 
-function AnalyticsComponent({ pathname }) {
-  return <Analytics data={{ pathname }} />;
-}
-
-export default AnalyticsComponent;
+export default Analytics;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
-
 import Analytics from "./Analytics";
+
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 function App() {
-  const location = useLocation();
   return (
-    <div id="appContainer" pathname={location.pathname}>
+    <div id="appContainer">
       <Navbar />
       <Analytics />
       <Footer />
