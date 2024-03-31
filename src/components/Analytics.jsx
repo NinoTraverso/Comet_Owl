83f21fdd5b1344668bb6108ac5c6bccf43b49884
NinoTraverso <njt98git@proton.me>
2024-03-31 17:01:00 +0000

    modified analyics component

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,6 +1,12 @@
+// Example Analytics component
+import React from "react";
 import { Analytics } from "@vercel/analytics/react";
 import { inject } from "@vercel/analytics";
 
 inject();
 
-export default Analytics;
+function AnalyticsComponent({ pathname }) {
+  return <Analytics data={{ pathname }} />;
+}
+
+export default AnalyticsComponent;
