3a45e1725b84eefb6c8bad6a21a490802fa1a143
NinoTraverso <njt98git@proton.me>
2024-03-31 16:41:51 +0000

    added analytics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import { Analytics } from "@vercel/analytics/react";
 
 import Navbar from "./Navbar";
 import Footer from "./Footer";
