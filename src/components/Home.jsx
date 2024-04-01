5bc5a42d706ef488d5810170bf91d39d053fa024
NinoTraverso <njt98git@proton.me>
2024-04-01 11:39:37 +0000

    copleted third section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,18 +25,43 @@ function Home() {
       <div className="welcomeText d-flex justify-content-center align-items-center">
         <h1>Welcome to CometOwl</h1>
       </div>
+      {/* ------------------------------------------------SECOND SECTION -------------------------------------------- */}
       <div className="d-flex flex-column  flex-xl-row justify-content-around align-items center">
-        <div className="pelicanNebulaImg align-self-center">
+        <div className="pelicanNebulaImg align-self-center rounded mx-5">
           <img
             src="./assets/space-images/northAmericanPelicanNebula.jpg"
             alt="North American and Pelican Nebula"
+            className="rounded"
           />
         </div>
         <div className="d-flex flex-column justify-content-center align-items-center my-5">
+          <div>
+            <img src="./assets/icons/satellite.svg" alt="" />
+          </div>
           <h1 className="w-75 text-center">
             At CometOwl we strive to give you the best Astronomy experience...
           </h1>
           <div className="loaderTwo my-5"></div>
+        </div>
+      </div>
+      <div className="separator"></div>
+      {/*---------------------------------------------- THIRD SECTION ----------------------------------------- */}
+      <div className="d-flex flex-column  flex-xl-row justify-content-around align-items center">
+        <div className="d-flex flex-column justify-content-center align-items-center my-5">
+          <div>
+            <img src="./assets/icons/rocket.svg" alt="" />
+          </div>
+          <h1 className="w-75 text-center">
+            Discover the Solar System and delve with us into the stars.
+          </h1>
+          <div className="loaderThree my-5"></div>
+        </div>
+        <div className="pelicanNebulaImg align-self-center rounded mx-5">
+          <img
+            src="./assets/space-images/spaceFlashlight.jpg"
+            alt="Nightsky and flashlight"
+            className="rounded"
+          />
         </div>
       </div>
     </div>
