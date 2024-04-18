beefb1087dbcc17f76a7a9c2374a6f0c2e24ba2a
NinoTraverso <njt98git@proton.me>
2024-04-18 05:14:24 +0000

    text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -326,9 +326,7 @@ function Home() {
                 <div className="d-flex flex-column justify-content-center align-items-center">
                   <h2>Ceres</h2>
                   <h6 className="w-50">
-                    Closest planet to the Sun, with an average surface
-                    temperature of 167&deg; C and orbiting the Sun every 88
-                    days.{" "}
+                    Second largest Kuiper belt object after Pluto.{" "}
                   </h6>
                   <ul>
                     <li>Radius: 2,439.7 km</li>
