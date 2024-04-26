2337752f300c16f6220f6c2b10c01670072401c9
NinoTraverso <njt98git@proton.me>
2024-04-26 22:40:27 +0000

    text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -299,9 +299,8 @@ function Home() {
                 <div className="d-flex flex-column justify-content-center align-items-center">
                   <h2>Neptune</h2>
                   <h6 className="w-50">
-                    Closest planet to the Sun, with an average surface
-                    temperature of 167&deg; C and orbiting the Sun every 88
-                    days.{" "}
+                    This is the other icy giant and last planet in our Solar
+                    System.{" "}
                   </h6>
                   <ul>
                     <li>Radius: 2,439.7 km</li>
