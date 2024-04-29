5ccf951718821276fa1bf7292ab0c3c933016b76
NinoTraverso <njt98git@proton.me>
2024-04-29 14:22:01 +0000

    mars text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -239,7 +239,7 @@ function Home() {
                   </h6>
                   <ul>
                     <li>Radius: 3,390 km</li>
-                    <li>Distance from Sun: 228 million km &#40;1.52 AU&#41;</li>
+                    <li>Distance from Sun: 1.52 AU&#41;</li>
                     <li>Composition: Iron, nickel and sulfur.</li>
                     <li>
                       Atmosphere: 95% Carbon Dioxide, 3% Nitrogen and other
