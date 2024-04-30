6de822714a75832fa4558d56b52041f9b89faaee
NinoTraverso <njt98git@proton.me>
2024-04-30 09:36:34 +0000

    starting footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,14 @@ const currentYear = new Date().getFullYear();
 function Footer() {
   return (
     <footer>
-      <h6 className="mx-3">Copyright &#169; {currentYear} - CometOwl</h6>
+      <div
+        id="footerLogo"
+        className="d-flex flex-column justify-content-center align-items-center py-3"
+      >
+        <img src="./assets/logo/logoAndName.png" alt="cometowl logo" />
+
+        <h4 className="mx-3">Copyright &#169; {currentYear} - CometOwl</h4>
+      </div>
     </footer>
   );
 }
