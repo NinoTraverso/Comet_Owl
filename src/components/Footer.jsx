17eb85f74935407de8e2bf4a25d470ac0820d94b
NinoTraverso <njt98git@proton.me>
2024-05-01 09:16:31 +0000

    footer structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,18 @@ const currentYear = new Date().getFullYear();
 function Footer() {
   return (
     <footer>
-      <div
-        id="footerLogo"
-        className="d-flex flex-column justify-content-center align-items-center py-3"
-      >
-        <img src="./assets/logo/logoAndName.png" alt="cometowl logo" />
+      <div className="d-flex flex-column flex-md-row justify-content-around align-items-center">
+        <div
+          id="footerLogo"
+          className="d-flex flex-column justify-content-center align-items-center py-3"
+        >
+          <img src="./assets/logo/logoAndName.png" alt="cometowl logo" />
 
-        <h4 className="mx-3">Copyright &#169; {currentYear} - CometOwl</h4>
+          <h4 className="mx-3">Copyright &#169; {currentYear} - CometOwl</h4>
+        </div>
+        <div>
+          <h1>Contact form</h1>
+        </div>
       </div>
     </footer>
   );
