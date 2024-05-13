dfa281d483df8e830635e7b28c146a2edd4c33c2
NinoTraverso <njt98git@proton.me>
2024-05-13 13:12:25 +0000

    created map for glossary (term name not showing) - needs fixing

--- src/components/Glossary.jsx
+++ src/components/Glossary.jsx
@@ -2,6 +2,24 @@ import React from "react";
 import Analytics from "./Analytics";
 import SpeedInsights from "./SpeedInsights";
 
+import GlossaryCard from "./GlossaryCard";
+import glossary from "../glossary";
+
+function createGlossaryCard(glossary) {
+  return (
+    <div
+      key={glossary.id}
+      className="col-12 col-xl-4 mb-5 d-flex justify-content-center"
+    >
+      <GlossaryCard
+        id={glossary.id}
+        termn={glossary.term}
+        description={glossary.description}
+      />
+    </div>
+  );
+}
+
 function Glossary() {
   return (
     <div>
@@ -9,7 +27,7 @@ function Glossary() {
       <SpeedInsights />
       <div className="nav-separator"></div>
 
-      <h1>GLOSSARY PAGE</h1>
+      <div className="row mb-5 mx-1">{glossary.map(createGlossaryCard)}</div>
     </div>
   );
 }
--- src/components/GlossaryCard.jsx
+++ src/components/GlossaryCard.jsx
@@ -6,7 +6,9 @@ function GlossaryCard(props) {
   return (
     <div id="glossaryCards" className={`${className}`}>
       <div className="d-flex flex-column justify-content-center aling-items-center">
-        <h1 className="term glossaryTerm text-center">{props.term}</h1>
+        <h5 className="term glossaryTerm text-center text-primary">
+          {props.term}
+        </h5>
 
         <h5 className="description glossaryDescription">{props.description}</h5>
       </div>
