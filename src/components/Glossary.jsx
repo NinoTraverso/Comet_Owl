3477becd7e7535f6a739e8c804e0a9101501e045
NinoTraverso <njt98git@proton.me>
2024-05-25 06:09:57 +0200

    text color

--- src/components/Glossary.jsx
+++ src/components/Glossary.jsx
@@ -26,7 +26,7 @@ function Glossary() {
       <Analytics />
       <SpeedInsights />
       <div className="nav-separator"></div>
-      <h1 className="text-info">Glossary List</h1>
+      <h1 className="text-secondary">Glossary List</h1>
 
       <div className="row mb-5 mx-1">{glossary.map(createGlossaryCard)}</div>
     </div>
