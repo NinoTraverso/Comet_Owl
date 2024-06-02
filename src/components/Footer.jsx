863b90fedb1ad9b1325d753a3a296e4e0aa7a847
NinoTraverso <njt98git@proton.me>
2024-06-02 22:44:52 +0200

    text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ function Footer() {
           <h4 className="mx-3">Copyright &#169; {currentYear} - CometOwl</h4>
         </div>
         <div className="d-flex flex-column text-center align-items-center">
-          <h3>Contact form</h3>
+          <h3>Contact</h3>
           <input
             type="text"
             placeholder="Name"
