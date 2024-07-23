a3409300dc6559e91515050167f773019f550959
NinoTraverso <njt98git@proton.me>
2024-07-23 12:48:10 +0200

    glossary search and filter function

--- src/components/Glossary.jsx
+++ src/components/Glossary.jsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Analytics from "./Analytics";
 import SpeedInsights from "./SpeedInsights";
-
 import GlossaryCard from "./GlossaryCard";
 import glossary from "../glossary";
 
@@ -21,14 +20,67 @@ function createGlossaryCard(glossary) {
 }
 
 function Glossary() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedLetter, setSelectedLetter] = useState("");
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+    setSelectedLetter("");
+  };
+
+  const handleLetterClick = (letter) => {
+    setSelectedLetter(letter);
+    setSearchQuery("");
+  };
+
+  const handleAllClick = () => {
+    setSelectedLetter("");
+    setSearchQuery("");
+  };
+
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+  const filteredGlossary = glossary.filter((item) => {
+    if (selectedLetter) {
+      return item.term.toLowerCase().startsWith(selectedLetter.toLowerCase());
+    }
+    return item.term.toLowerCase().includes(searchQuery.toLowerCase());
+  });
+
   return (
     <div>
       <Analytics />
       <SpeedInsights />
       <div className="nav-separator"></div>
       <h1 className="text-secondary">Glossary</h1>
-
-      <div className="row mb-5 mx-1">{glossary.map(createGlossaryCard)}</div>
+      <div className="d-flex flex-row">
+        <div className="mb-3 w-25">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search..."
+            value={searchQuery}
+            onChange={handleSearch}
+          />
+        </div>
+        <div className="mb-3">
+          <button className="btn btn-link" onClick={handleAllClick}>
+            All
+          </button>
+          {alphabet.map((letter) => (
+            <button
+              key={letter}
+              className="btn btn-link"
+              onClick={() => handleLetterClick(letter)}
+            >
+              {letter}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className="row mb-5 mx-1">
+        {filteredGlossary.map(createGlossaryCard)}
+      </div>
     </div>
   );
 }
