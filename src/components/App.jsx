dc7569c31b7e138a0687b516ebd8eef1c4167dab
NinoTraverso <njt98git@proton.me>
2024-09-06 04:44:56 +0200

    added react-helmet and SEO

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import { Helmet } from "react-helmet";
 import Analytics from "./Analytics";
 import SpeedInsights from "./SpeedInsights";
-
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 function App() {
   return (
     <div id="appContainer">
+      {/* Global SEO metadata using Helmet */}
+      <Helmet>
+        <title>CometOwl</title>
+        <meta name="description" content="Astronomy website" />
+        <meta
+          name="keywords"
+          content="astronomy, cometowl, CometOwl, space, cosmology, science, astrophysics, articles, exoplanets, planetary sciences, Astronomy Website"
+        />
+      </Helmet>
+
       <Navbar />
       <Analytics />
       <SpeedInsights />
