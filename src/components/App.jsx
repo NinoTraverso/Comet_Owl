053e4fd73bfccf26e158af7928b1ffaf11db6465
NinoTraverso <njt98git@proton.me>
2024-09-09 08:15:08 +0200

    metadata SEO improved

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,11 @@ function App() {
       {/* Global SEO metadata using Helmet */}
       <Helmet>
         <title>CometOwl</title>
-        <meta name="description" content="Astronomy website" />
+        <meta
+          name="description"
+          content="CometOwl is your ultimate resource for astronomy articles, space exploration, cosmology insights, and astrophysics. Discover the mysteries of the universe with our expert content."
+        />
+
         <meta
           name="keywords"
           content="astronomy, cometowl, CometOwl, space, cosmology, science, astrophysics, articles, exoplanets, planetary sciences, Astronomy Website"
