4d1b2bf04eda5fad4f40f324880dc77f468a8510
NinoTraverso <njt98git@proton.me>
2024-09-10 08:30:30 +0200

    adjusted UI in glossary and Exoplanet pages

--- src/components/GlossaryCard.jsx
+++ src/components/GlossaryCard.jsx
@@ -4,7 +4,10 @@ function GlossaryCard(props) {
   const className = `glossary glossary${props.id}`;
 
   return (
-    <div id="glossaryCards" className={`${className} rounded`}>
+    <div
+      id="glossaryCards"
+      className={`${className} rounded d-flex justify-content-center`}
+    >
       <div className="d-flex flex-column justify-content-center aling-items-center ">
         <h2 className="term glossaryTerm text-center mx-2 my-1">
           {props.term}
