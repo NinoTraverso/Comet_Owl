4545c6f0bfcc07e820441b2f1f6893544f1ce179
NinoTraverso <njt98git@proton.me>
2024-09-11 09:30:43 +0200

    simplified form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -29,42 +29,15 @@ export const ContactUs = () => {
 
   return (
     <form ref={form} onSubmit={sendEmail}>
-      <div
-        id="contactFormContainer"
-        className="d-flex flex-column justify-content-center align-items-start p-3"
-      >
-        <div className="align-self-center">
-          <img src="/assets/iconEmail.svg" alt="" />
-        </div>
-        <input
-          className="inputName w-50 my-4 rounded"
-          type="text"
-          name="user_name"
-          placeholder="  Your name"
-          required
-        />
-        <input
-          className="inputEmail w-50 my-4 rounded"
-          type="email"
-          name="user_email"
-          placeholder="  Your email"
-          required
-        />
-        <textarea
-          className="inputMessage w-75 my-4 rounded"
-          name="message"
-          maxLength="500"
-          placeholder="  Write your message..."
-          required
-        />
-
-        <input
-          id="submitButton"
-          className="align-self-start pt-2 pb-2 px-4 "
-          type="submit"
-          value="Send"
-        />
-      </div>
+      <input type="text" name="user_name" placeholder="Your name" required />
+      <input type="email" name="user_email" placeholder="Your email" required />
+      <textarea
+        name="message"
+        maxLength="500"
+        placeholder="Write your message..."
+        required
+      />
+      <input type="submit" value="Send" />
     </form>
   );
 };
