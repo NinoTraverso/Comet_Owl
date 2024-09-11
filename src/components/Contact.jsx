c4c5d2ea6796e01b4bdc505fe5d712be07a6c075
NinoTraverso <njt98git@proton.me>
2024-09-11 10:59:38 +0200

    bug

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ import { ContactUs } from "./ContactForm"; // Adjust the path if necessary
 const Contact = () => {
   return (
     <div>
-      <h1>Contact Us</h1>
+      <h1>Contact</h1>
       <ContactUs />
     </div>
   );
