3e9e5f1276602fcb2151b61d7c2a9bacef4dc994
NinoTraverso <njt98git@proton.me>
2024-09-11 08:37:48 +0200

    attempting with another code that worked on moble

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -28,42 +28,41 @@ export const ContactUs = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail} className="contact-form">
+    <form ref={form} onSubmit={sendEmail}>
       <div
         id="contactFormContainer"
-        className="d-flex flex-column justify-content-center align-items-center p-3 mx-auto"
-        style={{ width: "100%", maxWidth: "600px" }}
+        className="d-flex flex-column justify-content-center align-items-start p-3"
       >
+        <div className="align-self-center">
+          <img src="/assets/iconEmail.svg" alt="" />
+        </div>
         <input
-          className="inputName w-100 my-4 rounded"
+          className="inputName w-50 my-4 rounded"
           type="text"
           name="user_name"
           placeholder="  Your name"
           required
-          style={{ padding: "10px", fontSize: "16px" }}
         />
         <input
-          className="inputEmail w-100 my-4 rounded"
+          className="inputEmail w-50 my-4 rounded"
           type="email"
           name="user_email"
           placeholder="  Your email"
           required
-          style={{ padding: "10px", fontSize: "16px" }}
         />
         <textarea
-          className="inputMessage w-100 my-4 rounded"
+          className="inputMessage w-75 my-4 rounded"
           name="message"
           maxLength="500"
           placeholder="  Write your message..."
           required
-          style={{ padding: "10px", fontSize: "16px", minHeight: "150px" }}
         />
+
         <input
-          id="contactFormButton"
-          className="align-self-center pt-2 pb-2 my-4 px-4 btn btn-primary"
+          id="submitButton"
+          className="align-self-start pt-2 pb-2 px-4 "
           type="submit"
           value="Send"
-          style={{ width: "100%", fontSize: "18px" }}
         />
       </div>
     </form>
