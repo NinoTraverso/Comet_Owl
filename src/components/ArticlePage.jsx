9b5a51a99d81bfd7d3e96adf8bee897d8e1414e3
NinoTraverso <njt98git@proton.me>
2025-02-14 23:26:34 +0000

    updated article page

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -20,24 +20,37 @@ function ArticlePage() {
     return <h2 className="text-center my-5">Article not found</h2>;
   }
 
+  // Ensure frontImage is an absolute URL
+  const absoluteImageUrl = article.frontImage.startsWith("http")
+    ? article.frontImage
+    : `https://yourwebsite.com${article.frontImage}`;
+
   return (
     <div id="articleCard" className={`article article${article.id} container`}>
       <Helmet>
+        {/* SEO & Open Graph Meta Tags */}
         <title>{article.title} | CometOwl</title>
         <meta name="description" content={article.briefing} />
+
+        {/* Open Graph (Facebook, LinkedIn, etc.) */}
         <meta property="og:title" content={article.title} />
         <meta property="og:description" content={article.briefing} />
-        <meta property="og:image" content={article.frontImage} />
+        <meta property="og:image" content={absoluteImageUrl} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <meta
           property="og:url"
           content={`https://cometowl.com/articles/${article.id}`}
         />
         <meta property="og:type" content="article" />
+
+        {/* Twitter Card */}
         <meta name="twitter:title" content={article.title} />
         <meta name="twitter:description" content={article.briefing} />
-        <meta name="twitter:image" content={article.frontImage} />
+        <meta name="twitter:image" content={absoluteImageUrl} />
         <meta name="twitter:card" content="summary_large_image" />
       </Helmet>
+
       <div className="nav-separator"></div>
       <div className="row justify-content-center">
         <div className="col-12 col-xl-8">
@@ -45,19 +58,22 @@ function ArticlePage() {
             <h1 className="name articleName text-center my-4">
               {article.title}
             </h1>
+
             {article.frontImage && (
               <div id="frontImage" className="image-container my-3">
                 <img
-                  src={article.frontImage}
+                  src={absoluteImageUrl}
                   alt={article.title}
                   className="article-image"
                 />
               </div>
             )}
+
             <h5 className="details articleDetails my-5">
               {article.introduction}
             </h5>
             <h5 className="details articleDetails my-5">{article.textOne}</h5>
+
             <div className="details articleDetails clearfix my-5">
               {article.illustrationOne && (
                 <img
@@ -68,6 +84,7 @@ function ArticlePage() {
               )}
               <h5>{article.textTwo}</h5>
             </div>
+
             <div className="details articleDetails clearfix my-5">
               {article.illustrationTwo && (
                 <img
@@ -78,7 +95,9 @@ function ArticlePage() {
               )}
               <h5>{article.textThree}</h5>
             </div>
+
             <h5 className="details articleDetails my-5">{article.textFour}</h5>
+
             {article.conclusionImage && (
               <div className="details articleDetails d-flex justify-content-center my-5">
                 <img
@@ -88,10 +107,12 @@ function ArticlePage() {
                 />
               </div>
             )}
+
             <h5 className="details articleDetails d-flex justify-content-center my-5">
               {article.conclusion}
             </h5>
             <h5 className="details articleDetails mb-4">{article.author}</h5>
+
             {article.references && (
               <>
                 <h4 className="text-secondary">References</h4>
@@ -103,6 +124,8 @@ function ArticlePage() {
           </div>
         </div>
       </div>
+
+      {/* Related Articles */}
       <div className="text-decoration-none">
         <h3 className="">Related Articles</h3>
         <ul>
