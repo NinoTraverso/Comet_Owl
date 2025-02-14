410b850d7c78157b1f682fbabfacfae95b1f15c8
NinoTraverso <njt98git@proton.me>
2025-02-14 23:04:57 +0000

    articlePage settings

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -16,6 +16,10 @@ function ArticlePage() {
     }
   }, [id]);
 
+  const fullImageUrl = article.frontImage.startsWith("http")
+    ? article.frontImage
+    : `https://cometowl.com${article.frontImage}`;
+
   if (!article) {
     return <h2 className="text-center my-5">Article not found</h2>;
   }
@@ -25,19 +29,24 @@ function ArticlePage() {
       <Helmet>
         <title>{article.title} | CometOwl</title>
         <meta name="description" content={article.briefing} />
+
+        {/* Open Graph (Facebook, LinkedIn) */}
         <meta property="og:title" content={article.title} />
         <meta property="og:description" content={article.briefing} />
-        <meta property="og:image" content={article.frontImage} />
+        <meta property="og:image" content={fullImageUrl} />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
+        <meta property="og:image:type" content="image/jpeg" />
         <meta
           property="og:url"
           content={`https://cometowl.com/articles/${article.id}`}
         />
         <meta property="og:type" content="article" />
+
+        {/* Twitter Card */}
         <meta name="twitter:title" content={article.title} />
         <meta name="twitter:description" content={article.briefing} />
-        <meta name="twitter:image" content={article.frontImage} />
+        <meta name="twitter:image" content={fullImageUrl} />
         <meta name="twitter:card" content="summary_large_image" />
       </Helmet>
 
