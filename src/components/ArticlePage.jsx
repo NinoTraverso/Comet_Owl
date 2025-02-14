db386e4903a4ccc25e6bee9f49a495fb6f3e4c29
NinoTraverso <njt98git@proton.me>
2025-02-14 23:45:18 +0000

    original article page

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -7,7 +7,6 @@ function ArticlePage() {
   const [searchParams] = useSearchParams();
   const id = searchParams.get("id");
 
-  // Find the article by ID
   const article = articles.find((a) => a.id === parseInt(id));
 
   useEffect(() => {
@@ -21,35 +20,24 @@ function ArticlePage() {
     return <h2 className="text-center my-5">Article not found</h2>;
   }
 
-  // Ensure absolute URL for images
-  const absoluteImageUrl = article.frontImage.startsWith("http")
-    ? article.frontImage
-    : `https://www.cometowl.com${article.frontImage}`;
-
   return (
     <div id="articleCard" className={`article article${article.id} container`}>
       <Helmet>
-        {/* Meta Tags for Social Media */}
         <title>{article.title} | CometOwl</title>
         <meta name="description" content={article.briefing} />
-
-        {/* Open Graph (Facebook, LinkedIn) */}
         <meta property="og:title" content={article.title} />
         <meta property="og:description" content={article.briefing} />
-        <meta property="og:image" content={absoluteImageUrl} />
+        <meta property="og:image" content={article.frontImage} />
         <meta
           property="og:url"
-          content={`https://www.cometowl.com/articles/${article.id}`}
+          content={`https://cometowl.com/articles/${article.id}`}
         />
         <meta property="og:type" content="article" />
-
-        {/* Twitter Card */}
         <meta name="twitter:title" content={article.title} />
         <meta name="twitter:description" content={article.briefing} />
-        <meta name="twitter:image" content={absoluteImageUrl} />
+        <meta name="twitter:image" content={article.frontImage} />
         <meta name="twitter:card" content="summary_large_image" />
       </Helmet>
-
       <div className="nav-separator"></div>
       <div className="row justify-content-center">
         <div className="col-12 col-xl-8">
@@ -57,58 +45,66 @@ function ArticlePage() {
             <h1 className="name articleName text-center my-4">
               {article.title}
             </h1>
-
-            {/* Front Image */}
             {article.frontImage && (
               <div id="frontImage" className="image-container my-3">
                 <img
-                  src={absoluteImageUrl}
+                  src={article.frontImage}
                   alt={article.title}
                   className="article-image"
                 />
               </div>
             )}
-
-            {/* Briefing */}
             <h5 className="details articleDetails my-5">
               {article.introduction}
             </h5>
-
-            {/* Sections of the article */}
             <h5 className="details articleDetails my-5">{article.textOne}</h5>
-            {article.illustrationOne && (
-              <img
-                src={article.illustrationOne}
-                alt="Illustration One"
-                className="illustrationOne"
-              />
-            )}
-            <h5 className="details articleDetails my-5">{article.textTwo}</h5>
-            {article.illustrationTwo && (
-              <img
-                src={article.illustrationTwo}
-                alt="Illustration Two"
-                className="illustrationTwo"
-              />
-            )}
-            <h5 className="details articleDetails my-5">{article.textThree}</h5>
+            <div className="details articleDetails clearfix my-5">
+              {article.illustrationOne && (
+                <img
+                  src={article.illustrationOne}
+                  alt="Illustration One"
+                  className="illustrationOne"
+                />
+              )}
+              <h5>{article.textTwo}</h5>
+            </div>
+            <div className="details articleDetails clearfix my-5">
+              {article.illustrationTwo && (
+                <img
+                  src={article.illustrationTwo}
+                  alt="Illustration Two"
+                  className="illustrationTwo"
+                />
+              )}
+              <h5>{article.textThree}</h5>
+            </div>
+            <h5 className="details articleDetails my-5">{article.textFour}</h5>
             {article.conclusionImage && (
-              <img
-                src={article.conclusionImage}
-                alt="Conclusion"
-                className="conclusionImage"
-              />
+              <div className="details articleDetails d-flex justify-content-center my-5">
+                <img
+                  src={article.conclusionImage}
+                  alt="Conclusion"
+                  className="conclusionImage"
+                />
+              </div>
             )}
-            <h5 className="details articleDetails my-5">
+            <h5 className="details articleDetails d-flex justify-content-center my-5">
               {article.conclusion}
             </h5>
             <h5 className="details articleDetails mb-4">{article.author}</h5>
+            {article.references && (
+              <>
+                <h4 className="text-secondary">References</h4>
+                <p className="details articleDetails d-flex justify-content-center my-5 text-secondary">
+                  {article.references}
+                </p>
+              </>
+            )}
           </div>
         </div>
       </div>
-
       <div className="text-decoration-none">
-        <h3>Related Articles</h3>
+        <h3 className="">Related Articles</h3>
         <ul>
           {articles
             .filter((a) => a.id !== article.id)
