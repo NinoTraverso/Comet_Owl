decf78f7c1955854575268a3731b7bdbe6d2df1c
NinoTraverso <njt98git@proton.me>
2025-02-15 07:19:59 +0000

    navbar disappears at scroll-down

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import Home from "./Home";
@@ -10,10 +10,33 @@ import More from "./More.jsx";
 import Contact from "./Contact.jsx";
 
 function Navbar() {
+  const [isVisible, setIsVisible] = useState(true);
+  const lastScrollY = useRef(window.scrollY);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > lastScrollY.current && currentScrollY > 50) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+      lastScrollY.current = currentScrollY;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="mb-5">
       <BrowserRouter>
-        <nav className="d-none d-xl-flex flex-row jusify-content-start align-items-center fixed-top ">
+        <nav
+          className={`fixed-top transition-all ${
+            isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
+          } d-flex flex-row justify-content-start align-items-center`}
+          style={{ transition: "opacity 0.3s ease-in-out" }}
+        >
           <div id="NavLogoContainer">
             <Link to="/Home">
               <img src="/assets/logo/logoAndName.png" alt="Comet Owl Logo" />
@@ -40,117 +63,11 @@ function Navbar() {
             </Link>
           </div>
         </nav>
-        <nav
-          id="collapseNavbar"
-          className=" navbar navbar-dark d-flex d-xl-none"
-        >
-          <div className="container-fluid d-flex flex-column flex-md-row justify-content-between">
-            <div id="NavLogoContainer">
-              <Link to="/Home">
-                <img src="/assets/logo/logoAndName.png" alt="Comet Owl Logo" />
-              </Link>
-            </div>
-            <button
-              className="navbar-toggler align-self-end align-self-md-center"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to="/Home"
-                    onClick={() =>
-                      document
-                        .querySelector(".navbar-collapse")
-                        .classList.remove("show")
-                    }
-                  >
-                    <h4 className="mx-3">Home</h4>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to="/Articles"
-                    onClick={() =>
-                      document
-                        .querySelector(".navbar-collapse")
-                        .classList.remove("show")
-                    }
-                  >
-                    <h4 className="mx-3">Articles</h4>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to="/Glossary"
-                    onClick={() =>
-                      document
-                        .querySelector(".navbar-collapse")
-                        .classList.remove("show")
-                    }
-                  >
-                    <h4 className="mx-3">Glossary</h4>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to="/Exoplanets"
-                    onClick={() =>
-                      document
-                        .querySelector(".navbar-collapse")
-                        .classList.remove("show")
-                    }
-                  >
-                    <h4 className="mx-3">Exoplanets</h4>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to="/More"
-                    onClick={() =>
-                      document
-                        .querySelector(".navbar-collapse")
-                        .classList.remove("show")
-                    }
-                  >
-                    <h4 className="mx-3">More</h4>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to="/Contact"
-                    onClick={() =>
-                      document
-                        .querySelector(".navbar-collapse")
-                        .classList.remove("show")
-                    }
-                  >
-                    <h4 className="mx-3">Contact</h4>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
         <Routes>
           <Route index element={<Home />} />
           <Route path="/Home" element={<Home />} />
           <Route path="/Articles" element={<Articles />} />
           <Route path="/articles/:title" element={<ArticlePage />} />
-
           <Route path="/Glossary" element={<Glossary />} />
           <Route path="/Exoplanets" element={<Exoplanets />} />
           <Route path="/More" element={<More />} />
